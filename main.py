5ac925664194cb5ad1ce00fdb17368143a87ed75
Nikita Povelkin <nikita.povelkin@hdi.global>
2025-08-15 10:52:40 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        dtSeconds = clock.tick(60)
+        dt = dtSeconds / 1000.0
+
+if __name__ == "__main__":
+    main()
